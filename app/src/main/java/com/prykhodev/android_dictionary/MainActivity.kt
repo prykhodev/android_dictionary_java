1428a5291ceb395476e378c197b4feffcc1b6cab
Ivan Prykhodko <workprykhodkoivan@gmail.com>
2021-12-18 20:09:13 +0200

    First commit

---
+++ app/src/main/java/com/prykhodev/android_dictionary/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.prykhodev.android_dictionary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.prykhodev.android_dictionary.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/prykhodev/android_dictionary/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.prykhodev.android_dictionary.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
